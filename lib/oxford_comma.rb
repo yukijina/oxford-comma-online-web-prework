5982c912254e8a28e1074d4d4ee9053b8c6301ba
Yuki N <lunarbase@outlook.com>
2018-10-31 21:02:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join(", ")
+    
+    elsif array.size == 2
+    array.insert(1,"and")
+    array.join(" ")
+    
+    elsif array.size == 3
+    array1 = []
+    array1.push(array[0], array[1])
+    str1 = array1.join(", ")
+    
+    array2 =[array[2]]
+    array2.insert(0, ", and")
+    str2 = array2.join(" ")
+    str1.concat(str2)
+    
+    else
+      
+  end
 end
